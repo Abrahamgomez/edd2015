65f33c36e46b29205a3b3ecd95a20c7515160105
Abraham Gomez Perez <abrahamgp@localhost.localdomain>
2015-10-13 12:47:34 -0500

    Agregar matrizNxN

---
+++ matrices3x3/matrizNxN.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int dim1, dim2;
+	
+	printf("Ingresa los datos de tu matriz NxN: ");
+	scanf("[%d , %d]", &dim1, &dim2);
+	
+	
+	
+	
+	
+	return 0;
+}
