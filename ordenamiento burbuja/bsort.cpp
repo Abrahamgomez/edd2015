614befffe623eff3720abfab4c0dec4d3472354a
Abraham Gomez Perez <abrahamgp@localhost.localdomain>
2015-12-16 01:32:47 -0600

    programa de ordenamiento burbuja

---
+++ ordenamiento burbuja/bsort.cpp
@@ -0,0 +1,68 @@
+// ORDENAMIENTO BURBUJA
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+#include <ctime>
+
+void generar (int *list, int n){
+	for (int i =0; i<n; i++)  list[i] =rand () %1000 +1;
+}
+
+void imprimir (int *list, int n){
+	for (int i =0; i<n; i++){
+
+	printf(" %6i ", list[i]);
+
+}
+printf("\n");
+}
+
+void swap(int &a, int &b){
+	int comosea= a;
+	a = b;
+	b= comosea;
+}
+
+long cont;
+
+void bsort(int *list, int n){
+
+    int j = 1;
+
+    while (j <= n){
+
+    cont++;
+
+    int i = 0;
+
+    while(i <= n-j-1){
+
+    if (list[i] > list [i+1])
+    swap (list[i], list[i+1]);
+
+    i++;
+   }
+    j++;
+  }
+}
+
+int main (){
+	int x;
+	srand (time(NULL));
+
+		int list[100];
+
+		generar(list,100);
+		imprimir(list,100);
+
+		cont = 0;
+		bsort(list,100);
+		imprimir(list,100);
+
+	return 0;
+}
+
+
+
+
+
